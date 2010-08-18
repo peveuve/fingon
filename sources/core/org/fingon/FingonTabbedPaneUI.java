b2e84fbd30d1354c5dbcf215818cc04ec4073712
peveuve <peveuve@localhost>
2010-08-18 14:55:58 +0200

    add tabbed pane support: says the title of the selected tab.

---
+++ sources/core/org/fingon/FingonTabbedPaneUI.java
@@ -0,0 +1,93 @@
+package org.fingon;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+import javax.swing.JComponent;
+import javax.swing.JTabbedPane;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.plaf.ComponentUI;
+import javax.swing.plaf.TabbedPaneUI;
+
+import org.fingon.synthesizer.SpeechSynthesizer;
+import org.fingon.synthesizer.SpeechSynthesizerFactory;
+import org.fingon.synthesizer.SynthesisException;
+
+public class FingonTabbedPaneUI extends TabbedPaneUI implements ChangeListener {
+    private static FingonTabbedPaneUI instance;
+    
+    public FingonTabbedPaneUI(JTabbedPane c) {
+    }
+
+    /**
+     * Returns the instance of UI
+     * @param c
+     * @return
+     */
+    public static ComponentUI createUI(JComponent c) {
+	if (instance == null) {
+	    instance = new FingonTabbedPaneUI((JTabbedPane)c);
+	}
+	return instance;
+    }
+    
+    /**
+     * @see javax.swing.plaf.ComponentUI#installUI(javax.swing.JComponent)
+     */
+    @Override
+    public void installUI(JComponent c) {
+	JTabbedPane tabbedPane = (JTabbedPane)c;
+	tabbedPane.addChangeListener(this);
+    }
+
+    /**
+     * @see javax.swing.plaf.ComponentUI#uninstallUI(javax.swing.JComponent)
+     */
+    @Override
+    public void uninstallUI(JComponent c) {
+	JTabbedPane tabbedPane = (JTabbedPane)c;
+	tabbedPane.removeChangeListener(this);
+    }
+
+    /**
+     * @see javax.swing.plaf.ComponentUI#update(java.awt.Graphics, javax.swing.JComponent)
+     */
+    @Override
+    public void update(Graphics g, JComponent c) {
+    }
+
+
+    @Override
+    public Rectangle getTabBounds(JTabbedPane arg0, int arg1) {
+	return null;
+    }
+
+    @Override
+    public int getTabRunCount(JTabbedPane pane) {
+	return 0;
+    }
+
+    @Override
+    public int tabForCoordinate(JTabbedPane pane, int x, int y) {
+	return 0;
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent event) {
+	JTabbedPane tabbedPane = (JTabbedPane)event.getSource();
+	if (tabbedPane.isShowing()) {
+	    int  selectedTabIndex = tabbedPane.getSelectedIndex();
+	    if (selectedTabIndex != -1) {
+    	    	String selectedTabTitle = tabbedPane.getTitleAt(selectedTabIndex);
+    	    	if (selectedTabTitle != null) {
+    	    	    try {
+    	    		SpeechSynthesizer synthesizer = SpeechSynthesizerFactory.getSpeechSynthesizer();
+    	    		synthesizer.stop();
+    	    		synthesizer.play(selectedTabTitle);
+    	    	    } catch (SynthesisException ex) {}
+    	    	}
+	    }
+	}
+    }
+}
